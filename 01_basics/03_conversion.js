5a4d1cbb351f0238409f333c02dc8967f4b19583
ankita322 <ankitamanna030202@gmail.com>
2025-05-02 15:27:20 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+// let score = false
+
+// console.log(typeof (score));
+
+// let newNum = Number(score)
+// console.log(typeof newNum);
+// console.log(newNum)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+// null = 0; undefined = NaN
+
+let isloggedIn = "ankita"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "ankita" => true
+
+let myNumber = 77
+let stringNumber = String(myNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
