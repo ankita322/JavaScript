42353ff3674e380f993384250860e1348cf24fa7
ankita322 <ankitamanna030202@gmail.com>
2025-05-02 14:39:55 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code s newer version
+
+// alert("hello") //we are using node JS not browser
+
+console.log(3+3)
+
+let name = "Ankita"
+let age = 13
+let loggedIN = false
+
+// number => 2 to power of 53
+// bigint 
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol = unique
+
+//object
+
+console.log(typeof "Ankita");
+console.log(typeof null);
